b975869ff462f2efcca2b277d6ce5525b93371c9
KaziMdMehediHasan <hassan.mehedee92@gmail.com>
2024-11-05 21:09:54 +0600

    code cleanup

--- src/app/modules/favouriteQuestion/favouriteQuestion.validation.ts
+++ src/app/modules/favouriteQuestion/favouriteQuestion.validation.ts
@@ -2,15 +2,6 @@ import { z } from "zod";
 
 
 const createFavouriteQuestionValidationSchema = z.object({
-    // body: z.object({
-    //     student_id: z.instanceof(mongoose.Types.ObjectId, {
-    //         message: 'Student ID must be provided'
-    //     }),
-    //     questions: z.array(z.instanceof(mongoose.Types.ObjectId), {
-    //         required_error: 'Provide an ObjectId',
-    //         message: 'Each item must be a valid ObjectId'
-    //     })
-    // })
     body: z.object({
         favourite_questions: z.string({
             required_error: 'Provide an ObjectId',
